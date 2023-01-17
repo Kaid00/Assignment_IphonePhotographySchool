4d0bf16e24d4e0cc5838814ef1c908a66440c6c1
Azamah Jr <depixellife@gmail.com>
2023-01-17 11:34:40 +0100

    Initial Commit

---
+++ Assignment/AssignmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  AssignmentApp.swift
+//  Assignment
+//
+//  Created by Azamah Junior Khan on 17/01/2023.
+//
+
+import SwiftUI
+
+@main
+struct AssignmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
