50fb5b8d747e2b4558f9704968dc5b7567003884
Azamah Jr <depixellife@gmail.com>
2023-01-20 15:21:26 +0100

    fix: waits for system to display components

--- Assignment_UITests/Lesson_UITests.swift
+++ Assignment_UITests/Lesson_UITests.swift
@@ -30,11 +30,11 @@ final class Lesson_UITests: XCTestCase {
         // Given
         let LessonList = app.navigationBars["Lessons"].staticTexts["Lessons"]
         let lesson = app.scrollViews.otherElements.buttons["The Key To Success In iPhone Photography"]
-        sleep(2)
+        sleep(10)
         lesson.tap()
         
         let backButton = app.navigationBars["_TtGC7SwiftUI32NavigationStackHosting"].buttons["Lessons"]
-        
+        sleep(1)
         backButton.tap()
         LessonList.tap()
 
@@ -44,6 +44,8 @@ final class Lesson_UITests: XCTestCase {
     
     func test_LessonDetails_nextLesson_shouldOpenToNextLesson() {
         let lesson =  app.scrollViews.otherElements.buttons["The Key To Success In iPhone Photography"]
+        
+        sleep(10)
         lesson.tap()
         
         let nextLessonButton = app.buttons["Next lesson"]
@@ -60,51 +62,61 @@ final class Lesson_UITests: XCTestCase {
     
     func test_LessonDetails_downloadButton_shouldShowCancelDownloadAndProgress() {
        
-        app.scrollViews.otherElements.buttons["The Key To Success In iPhone Photography"].tap()
+    
+        let lesson = app.scrollViews.otherElements.buttons["The Key To Success In iPhone Photography"]
+        
+        sleep(10)
+        lesson.tap()
         
         let detailNavButtons = app.navigationBars["_TtGC7SwiftUI32NavigationStackHosting"]
-        
+        sleep(1)
         detailNavButtons/*@START_MENU_TOKEN@*/.buttons["Download"]/*[[".otherElements[\"Download\"].buttons[\"Download\"]",".buttons[\"Download\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.tap()
 
         let cancelDownloadBtn = detailNavButtons/*@START_MENU_TOKEN@*/.buttons["Cancel download"]/*[[".otherElements[\"Cancel download\"].buttons[\"Cancel download\"]",".buttons[\"Cancel download\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/
 
-        XCTAssert(cancelDownloadBtn.waitForExistence(timeout: 30))
+        XCTAssert(cancelDownloadBtn.waitForExistence(timeout: 3))
 
         cancelDownloadBtn.tap()
         
-        let cancelDownloadBtnexist = cancelDownloadBtn.waitForExistence(timeout: 30)
+        let cancelDownloadBtnexist = cancelDownloadBtn.waitForExistence(timeout: 3)
         XCTAssertFalse(cancelDownloadBtnexist)
   
     }
     
     func test_LessonDetails_videoPlayer_videoPlayerShouldExist() {
         
-        app.scrollViews.otherElements.buttons["The Key To Success In iPhone Photography"].tap()
+       let lesson = app.scrollViews.otherElements.buttons["The Key To Success In iPhone Photography"]
+        sleep(10)
+        lesson.tap()
         
         let videoFrame = app.windows.children(matching: .other).element.children(matching: .other).element.children(matching: .other).element.children(matching: .other).element.children(matching: .other).element.children(matching: .other).element.children(matching: .other).element.children(matching: .other).element.children(matching: .other).element
+        
+        sleep(1)
         videoFrame.children(matching: .image).element(boundBy: 0).tap()
         
         let AVplayer = videoFrame.children(matching: .other).element(boundBy: 0).children(matching: .other).element
+        
+        sleep(1)
         AVplayer.children(matching: .other).element.children(matching: .other).element(boundBy: 1).tap()
         AVplayer.children(matching: .other).element(boundBy: 0).children(matching: .other).element(boundBy: 1).tap()
               
+        sleep(1)
         XCTAssertTrue(videoFrame.exists)
     }
     
     func test_LessonDetails_goToLessonsBtn_shouldGoToLessonsList() {
         
-        
-        let app = XCUIApplication()
         let scrollViewsQuery = app.scrollViews
-        let elementsQuery = scrollViewsQuery.otherElements
+        let elementsQuery = scrollViewsQuery.otherElements.buttons["Secrets For Capturing Beautiful iPhone Portrait Photos"]
         
-        sleep(3)
-        elementsQuery.buttons["Secrets For Capturing Beautiful iPhone Portrait Photos"].tap()
+        sleep(10)
+        elementsQuery.tap()
+        
         app.buttons["Back to lessons"].tap()
         
         let lessonsList = app.navigationBars["Lessons"].staticTexts["Lessons"]
         
-        XCTAssert(lessonsList.waitForExistence(timeout: 20))
+        XCTAssert(lessonsList.waitForExistence(timeout: 15))
         
        
                         
